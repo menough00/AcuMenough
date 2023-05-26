399c511623afec3f6947462905347a10deb95f34
menough00 <73511875+menough00@users.noreply.github.com>
2023-05-26 16:37:46 -0500

    Add project files.

---
+++ Controllers/LocationController.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AcuMenough.Models;
+using FluentValidation.Results;
+using System.Collections.Generic;
+using OfficeOpenXml;
+
+namespace AcuMenough.Controllers
+{
+    public class LocationController : Controller
+    {
+        private const string ExcelFilePath = @"C:\Users\nickm\source\repos\AcuMenough\wwwroot\data\floor.xlsx";
+
+        public ActionResult Index()
+        {
+            List<Location> locations = ReadLocationsFromExcel();
+            return View(locations);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Location location)
+        {
+            WriteLocationToExcel(location);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(string locationName)
+        {
+            Location location = ReadLocationsFromExcel().FirstOrDefault(l => l.LocationName == locationName);
+            return View(location);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Location location)
+        {
+            UpdateLocationInExcel(location);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(string locationName)
+        {
+            Location location = ReadLocationsFromExcel().FirstOrDefault(l => l.LocationName == locationName);
+            return View(location);
+        }
+
+        [HttpPost]
+        public ActionResult DeleteConfirmed(string locationName)
+        {
+            DeleteLocationFromExcel(locationName);
+            return RedirectToAction("Index");
+        }
+
+        private List<Location> ReadLocationsFromExcel()
+        {
+            List<Location> locations = new List<Location>();
+
+            using (ExcelPackage package = new ExcelPackage(new FileInfo(ExcelFilePath)))
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets[1];
+                int rowCount = worksheet.Dimension.Rows;
+
+                for (int row = 2; row <= rowCount; row++)
+                {
+                    locations.Add(new Location
+                    {
+                        LocationName = worksheet.Cells[row, 1].Value.ToString(),
+                        LocationId = int.Parse(worksheet.Cells[row, 2].Value.ToString()),
+                        IsClearance = worksheet.Cells[row, 3].Value.ToString()
+                    });
+                }
+            }
+
+            return locations;
+        }
+
+        private void WriteLocationToExcel(Location location)
+        {
+            using (ExcelPackage package = new ExcelPackage(new FileInfo(ExcelFilePath)))
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets[1];
+                int rowCount = worksheet.Dimension.Rows;
+
+                worksheet.Cells[rowCount + 1, 1].Value = location.LocationName;
+                worksheet.Cells[rowCount + 1, 2].Value = location.LocationId;
+                worksheet.Cells[rowCount + 1, 3].Value = location.IsClearance;
+
+                package.Save();
+            }
+        }
+
+        private void UpdateLocationInExcel(Location location)
+        {
+            using (ExcelPackage package = new ExcelPackage(new FileInfo(ExcelFilePath)))
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets[1];
+
+                int rowCount = worksheet.Dimension.Rows;
+                for (int row = 2; row <= rowCount; row++)
+                {
+                    if (worksheet.Cells[row, 1].Value.ToString() == location.LocationName)
+                    {
+                        worksheet.Cells[row, 2].Value = location.LocationId;
+                        worksheet.Cells[row, 3].Value = location.IsClearance;
+                        break;
+                    }
+                }
+
+                package.Save();
+            }
+        }
+
+        private void DeleteLocationFromExcel(string locationName)
+        {
+            using (ExcelPackage package = new ExcelPackage(new FileInfo(ExcelFilePath)))
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets[1];
+
+                int rowCount = worksheet.Dimension.Rows;
+                for (int row = 2; row <= rowCount; row++)
+                {
+                    if (worksheet.Cells[row, 1].Value.ToString() == locationName)
+                    {
+                        worksheet.DeleteRow(row);
+                        break;
+                    }
+                }
+
+                package.Save();
+            }
+        }
+    }
+}
---
+++ Models/Location.cs
@@ -0,0 +1,9 @@
+﻿namespace AcuMenough.Models
+{
+    public class Location
+    {
+        public string? LocationName { get; set; }
+        public int LocationId { get; set; }
+        public string? IsClearance { get; set; }
+    }
+}
